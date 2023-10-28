29e90eb050588a5e9f854620b7f917f257f507c9
hardikkum444 <hardikkumawat444@gmail.com>
2023-10-28 10:50:39 +0530

    star patterns

---
+++ star_pattern_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"enter: ";
+    int n;
+    cin>>n;
+    
+    for(int i=n;i>=0;i--)
+    {
+        for(int j =i-1;j>=0;j--)
+        {
+            cout<<"*";
+            
+        }
+        for(int k =i;k<=n;k++)
+        {cout<<" ";}
+        cout<<endl;
+    }
+    
+    
+}
