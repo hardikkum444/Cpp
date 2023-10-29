43908727618504b476d30fe0335327047c1a850d
hardikkum444 <hardikkumawat444@gmail.com>
2023-10-29 10:58:38 +0530

    protected added

--- inheritance_1.cpp
+++ inheritance_1.cpp
@@ -31,6 +31,13 @@ int main()
 }
 
 
+//defination of protected access specifier 
+//basically this means that you can use the members defined in this class
+//within this class, also you can use them in the class derived from this class
+//however you cannot use them anywhere else, mainly not in the driver function either
+ 
+
+
 
 
 
