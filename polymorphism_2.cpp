ae23f80d4b4203383fb04782a3380267e043dba8
hardikkum444 <hardikkumawat444@gmail.com>
2023-10-29 14:23:54 +0530

    prime exaple of operator overloading

---
+++ polymorphism_2.cpp
@@ -0,0 +1,29 @@
+//prime exaple of operator overloading
+
+#include <iostream>
+using namespace std;
+
+class ops
+{
+    public:
+    int a;
+
+    void operator+ (ops &obj)
+    {
+        int value1 = this->a;
+        int value2 = obj.a;
+        cout<<value1-value2<<endl;
+    }
+};
+
+
+int main()
+{
+    ops o1;
+    ops o2;
+
+    o1.a = 7;
+    o2.a = 2;
+     
+    o1+o2;
+}
